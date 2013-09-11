50ed013e3ffe172715cd8b635a741df9c9b993ca
boatgm <mg90@foxmail.com>
2013-09-11 18:24:16 +0800

    ready api

--- app.js
+++ app.js
@@ -24,35 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
-
 app.get('/', routes.index);
 app.get('/users', user.list);
-/*api*/
-app.get('/api/channel/list/', function(req, res) {
-	res.send([{
-		"id": "1",
-		"src": "photo path",
-		"intro": "photo description"
-	}, {
-		"id": "1",
-		"src": "photo path",
-		"intro": "photo description"
-	}, ]);
-});
-
-app.get('/api/channel/info/', function(req, res) {
-	res.send([{
-		"id": "1",
-		"src": "photo path",
-		"intro": "photo description"
-	}, {
-		"id": "1",
-		"src": "photo path",
-		"intro": "photo description"
-	}, ]);
-
-});
-
+app.get('/userlist',user.list);
+app.get('/',comment.list);
 var server = app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
